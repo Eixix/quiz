de45fd7d34137dda61363230367940b964963395
Eixix <tobiasbetz@hotmail.de>
2022-07-10 19:37:48 +0200

    Added vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,10 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -13,9 +17,11 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
+        const vuetify = createVuetify({components, directives});
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(vuetify)
             .mount(el);
     },
 });
